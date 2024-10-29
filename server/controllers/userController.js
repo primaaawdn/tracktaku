fd5424446be4762f9d6d2fcb49f2f9e6ed0bfe19
Prima <widianiprima@gmail.com>
2024-10-29 23:38:27 +0700

    feat(server): add feature to retrieve all user data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -57,3 +57,12 @@ exports.login = async (req, res, next) => {
         next(error);
 	}
 };
+
+exports.getAllUsers = async (req, res,next) => {
+	try {
+		const users = await User.findAll();
+		res.status(200).json(users);
+	} catch (error) {
+		next(error);
+	}
+}
--- server/routers/userRoutes.js
+++ server/routers/userRoutes.js
@@ -3,6 +3,7 @@ const userController = require("../controllers/userController");
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.get('/all', userController.getAllUsers);
 
 
 module.exports = router;
