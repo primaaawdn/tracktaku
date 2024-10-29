91340b477835e89fc30df343acb509fa0c8bf779
Prima <widianiprima@gmail.com>
2024-10-29 23:36:03 +0700

    feat(server): add login feature

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 const { User } = require("../models");
+const bcrypt = require("bcrypt");
+const { signToken } = require("../helpers/jwt");
 
 exports.register = async (req, res, next) => {
 	// console.log(req.body);
@@ -18,3 +20,40 @@ exports.register = async (req, res, next) => {
 	}
 };
 
+exports.login = async (req, res, next) => {
+	try {
+		const { email, password } = req.body;
+		// console.log(password, "login user controller");
+
+		if(!email) {
+			next ({ name: "BadRequest", message: `Email is required` });
+			return;
+		}
+
+		if(!password) {
+			next ({ name: "BadRequest", message: `Password is required` });
+			return;
+		}
+	
+		const user = await User.findOne({ where: { email }});
+
+		if (!user) {
+			next ({ name: "Unauthorized", message: `Invalid email` });
+			return;
+		}
+
+		const passwordMatch = await bcrypt.compare(password, user.password);
+
+		if (!passwordMatch) {
+			next ({ name: "Unauthorized", message: "Invalid password" });
+			return;
+		}
+
+		const access_token = signToken({ id: user.id });
+		// console.log("Generated access token:", access_token);
+		res.status(200).json({ access_token });
+	} catch (error) {
+		// console.error(error);
+        next(error);
+	}
+};
