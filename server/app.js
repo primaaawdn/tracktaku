d08367fce5a398b5b76d1e4c73dcb69a9695b09f
Prima <widianiprima@gmail.com>
2024-11-01 00:40:19 +0700

    feat(server): configuring app.js

--- server/app.js
+++ server/app.js
@@ -5,8 +5,9 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const { errorhandler } = require("./middlewares/errorhandler");
