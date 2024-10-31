0ec4667fbff0080d1ed5515d0b2843954edfcfef
Prima <widianiprima@gmail.com>
2024-10-31 19:43:23 +0700

    feat(server): edit route - adding reading list routes

--- server/app.js
+++ server/app.js
@@ -5,24 +5,26 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const cors = require('cors');
 const app = express();
-// const port = 3000;
+const port = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const { errorhandler } = require("./middlewares/errorhandler");
 
 const userRoutes = require("./routers/userRoutes");
+const readingListRoutes = require("./routers/readingListRoutes");
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
 // });
 
 app.use("/user", userRoutes);
+app.use("/manga", readingListRoutes);
 
 app.use(errorhandler);
 
-// app.listen(port, () => {
-// 	console.log(`Server running on port ${port}`);
-// });
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
 
 module.exports = app;
